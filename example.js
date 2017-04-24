166ada8d251a62dc9e64dfe1bb81794836d0b767
cuongurus <cuongnd95@live.com>
2017-04-25 04:48:08 +0700

    fix example

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
-var aloha = require('aloha-sd')
+var aloha = require('./')
 
 aloha.find((err, result) =>{
     if (err) console.log(err)
 
     if(result){
-        console.log("Found service: "+ JSON.stringify(result,null,4))
+        console.log(JSON.stringify(result,null,4))
     }
 },'_http._tcp')
