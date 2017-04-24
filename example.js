4bd06785fc91b12f720fbdfcbccf8fdbb6264f26
cuongurus <cuongnd95@live.com>
2017-04-25 05:10:12 +0700

    fix shutdown

--- example.js
+++ example.js
@@ -1,9 +1,15 @@
 var aloha = require('./')
 
-aloha.find((err, result) =>{
+var finder = aloha.find((err, result) =>{
     if (err) console.log(err)
 
     if(result){
         console.log(JSON.stringify(result,null,4))
     }
 },'_http._tcp')
+
+
+setTimeout(function(){
+    finder.shutdown()
+},3000)
+
